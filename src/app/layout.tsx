63451d9375372fc35d41931af4a2bd2b2bcbd871
NIGHTFURY609 <121104372+NIGHTFURY609@users.noreply.github.com>
2025-05-13 16:37:24 +0530

    trying to reduce lcp again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <div className="container mx-auto px-4 py-10 flex-grow">
           {/* Header */}
           <header className="mb-10 text-center">
-            <h1 className="text-4xl font-semibold text-secondary tracking-tight">
+            <h1 className="text-4xl font-semibold text-secondary ">
               Search For Random Users
             </h1>
             <p className="text-gray-600 mt-2 text-lg">Find and explore random people worldwide</p>
